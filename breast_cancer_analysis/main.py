a6a87393c31544ac67d31bd075857e8d20ba1d19
Anup Kumar <anup.rulez@gmail.com>
2024-09-09 09:41:19 +0000

    update scripts

--- breast_cancer_analysis/main.py
+++ breast_cancer_analysis/main.py
@@ -1,35 +1,35 @@
 import preprocess_data
 import train_model
-import xai_explainer
+#import xai_explainer
 
 
 config = {
     "SEED": 32,
-    "n_edges": 2000000,
+    "n_edges": 1500000,
     "n_epo": 4,
     "k_folds": 5,
     "batch_size": 128,
     "num_classes": 5,
-    "gene_dim": 40,
+    "gene_dim": 86,
     "hidden_dim": 128,
     "learning_rate": 0.0001,
     "scale_features": "0,1,3",  #"degree,ring,NetShort",
-    "out_links": "../../pu_label_propagation/data/output/out_links.csv",
-    "out_genes": "../../pu_label_propagation/data/output/out_genes.csv",
-    "out_gene_rankings": "../../pu_label_propagation/data/output/out_gene_rankings.csv",
-    "merged_signals": "../../process_illumina_arrays/data/output/merged_signals.csv",
-    "nedbit_features": "../../pu_label_propagation/data/output/nedbit_features.csv",
-    "dnam_features": "../../pu_label_propagation/data/output/dnam_features.csv",
-    "nedbit_dnam_features": "../data/output/df_nebit_dnam_features.csv",
-    "nedbit_dnam_features_norm": "../data/output/df_nebit_dnam_features_norm.csv",
-    "plot_local_path": "../data/output/",
-    "data_local_path": "../data/output/",
-    "model_local_path": "../model/"
+    "out_links": "data/out_links_bc.csv",
+    "out_genes": "data/out_genes_bc.csv",
+    "out_gene_rankings": "data/out_gene_rankings_bc.csv",
+    "merged_signals": "data/combined_pos_neg_signals_bc.csv",
+    "nedbit_features": "data/nedbit_features_bc.csv",
+    "dnam_features": "data/dnam_features_bc.csv",
+    "nedbit_dnam_features": "data/df_nebit_dnam_features_bc.csv",
+    "nedbit_dnam_features_norm": "data/df_nebit_dnam_features_norm.csv",
+    "plot_local_path": "data/",
+    "data_local_path": "data/",
+    "model_local_path": "model/"
 }
 
 
 def run_training():
-    #preprocess_data.read_files(config)
+    preprocess_data.read_files(config)
     train_model.train_gnn_model(config)
     #xai_explainer.gnn_explainer(trained_model, data, config)
 
